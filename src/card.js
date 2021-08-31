a58396a090071eebb47ba27597fc4dc22dc90dd1
Nedim <mr.ramicevic@gmail.com>
2021-08-31 10:58:19 +0300

    bootstrap

--- src/card.js
+++ src/card.js
@@ -1,4 +1,4 @@
-import React,{useReducer,useEffect} from 'react'
+import React,{useReducer,useState} from 'react'
 
 const reducer = (state,action) =>{
     if (action.type === "inc") {
@@ -6,6 +6,7 @@ const reducer = (state,action) =>{
     }
   }
 const reducer1 = (state,name) =>{
+
     return {...state, name: "naber"}
 }
 const init = 0
@@ -14,16 +15,17 @@ const Card = ({card,onChangeInput,onDelete}) => {
 
     const [count, dispatch] = useReducer(reducer, init)
     const [state, dispatch1] = useReducer(reducer1, card)
+
     return (
         <div className="card">
         <div className="container">
           <h4>
-            <b>{state.name}</b>
+            <b>{card.name}</b>
           </h4>
           <input
             type="text"
             onChange={onChangeInput}
-            defaultValue = {state.name}
+            defaultValue = {card.name}
           ></input>
           <p>{card.email}</p>
           <div className="btn">
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,25 @@
-import React from 'react'
-import faker from 'faker'
+import React,{useState,useEffect} from 'react'
 
 const Home = () => {
+    const [posts, setPosts] = useState()
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(json => setPosts(json.slice(0,6)) )
+    }, [])
     return (
-        <div className="container">
-            <h4 className="center">Home</h4>
-            <p>{faker.lorem.paragraph()}</p>
+        <div className="container ">
+            <h4 className="center ">Home</h4>
+            <div className="row row-cols-3">
+            {posts ? posts.map(post =>(
+                <div class="card col" style={{backgroundColor: "#eee"}}>
+                <div class="card-body shadow">
+                  <h5 class="card-title">{post.title}</h5>
+                  <p class="card-text">{post.body}</p>
+                </div>
+                </div>
+            ) ):null}
+            </div>
         </div>
     )
 }
